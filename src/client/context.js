945d3b51723ab3001f586613286a3364e518d774
Filip Krawczyk <kotmawszy@gmail.com>
2023-07-28 22:45:02 +0200

    Add error throwing to usePagination

--- src/client/context.js
+++ src/client/context.js
@@ -7,7 +7,11 @@ import React, { createContext, useContext } from 'react';
 
 export const Context = createContext(null);
 
-export const usePagination = () => useContext(Context);
+export const usePagination = () => {
+    const context = useContext(Context);
+    if(context === null) throw new Error('usePagination must be used within a component wrapped in withPagination function');
+    return context;
+}
 
 export function ContextProvider({ data, children }) {
     return <Context.Provider value={data}>{children}</Context.Provider>;
